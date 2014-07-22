6858ead0e58997684d252e16cd28d74ddbb1c32d
parsah <parsa.hosseini@nih.gov>
2014-07-22 17:55:53 -0400

    modified proximal_analysis script to handle BED files.

--- proximal_analysis.py
+++ proximal_analysis.py
@@ -24,18 +24,19 @@ def map_features(b, gtf):
     @return: list referencing most proximal GTF entry for each BED entry.
     '''
 
-    genes, distance = [], []
+    hit_start, hit_end, distance = [], [], []
     fname = b.get_file()
-    cmd = 'bedtools closest -io -d -a ' + fname + ' -b ' + gtf + ' -t first'
+    cmd = 'bedtools closest -d -a ' + fname + ' -b ' + gtf + ' -t first'
     out, err = Popen(cmd, shell=True, stdout=PIPE).communicate()
     if err:  # stdout, stderr are outputs; stderr must be None
         raise IOError('Error found following mapping features to BED')
     for l in out.splitlines():
         l = l.decode('utf-8').split('\t')
-        gene = l[-2].split(';')[0].replace('gene_id', '').replace('"', "")
-        genes.append(gene.strip())  # add the entrie's most proximal gene
+        hit_start.append(int(l[-3]))  # append the start and end bases
+        hit_end.append(int(l[-2]))
         distance.append(int(l[-1]))  # add most proximal distance
-    b.get_data()['Gene_ID'] = genes  # add new columns to the data-structure
+    b.get_data()['Hit_Start'] = hit_start  # add new columns to data-structure
+    b.get_data()['Hit_End'] = hit_end  # add new columns to data-structure
     b.get_data()['Distance'] = distance
     ints = list(range(0, 200000, 10000))  # fit distances to intervals
     b.get_data()['Interval'] = [bisect_left(ints, pos) for pos in distance]
@@ -44,14 +45,13 @@ def map_features(b, gtf):
 def map_signals(b):
     '''
     '''
-    assert 'Gene_ID' in b.get_data().columns  # a Gene_ID columns is required
     signal = []
     for _, row in b.get_data().iterrows():  # row-number, row, respectively
         reps = []  # store bigwig replicate-averages across bigwig files
         for bigwig in b.get_bigwigs():  # compute BED average given bigwig
-            cmd = 'echo ' + str(row['Chr']) + ' ' + str(row['Start']) + ' ' +\
-                str(row['End']) + ' 1 | bigWigAverageOverBed ' + bigwig +\
-                 ' stdin stdout'
+            cmd = 'echo ' + str(row['Chr']) + ' ' + str(row['Hit_Start']) +\
+                ' ' + str(row['Hit_End']) + ' 1 | bigWigAverageOverBed ' +\
+                bigwig + ' stdin stdout'
             out, err = Popen(cmd, shell=True, stdout=PIPE).communicate()
             if err:  # stdout, stderr are outputs; stderr must be None
                 raise IOError('Error found following mapping features to BED')
@@ -63,9 +63,9 @@ def map_signals(b):
 def main(args):
     beds = [BEDFileFactory(elm).build() for elm in parse_config(args['in'])]
     bed_objs = [b for b in beds]  # merge BEDs into one structure
-    print('Chr,Length,Tissue,Class,GeneID,Distance,Interval,Signal')  # header
+    print('Chr,Length,Tissue,Class,Distance,Interval,Hit_Start,Hit_End,Signal')
     for b in bed_objs:
-        map_features(b, args['gtf'])
+        map_features(b, args['bed'])
         if args['signals']:  # map signals (BigWig files), if need-be
             map_signals(b)
         else:
@@ -73,18 +73,20 @@ def main(args):
         data = b.get_data()
         for _, row in data.iterrows():
             if row['Distance'] >= 0:  # BED entry lacks proximal, i.e. chrX
+                pass
                 print(row['Chr'] + ',' + str(row['Length']) + ',' +
                       str(row['Tissue']) + ',' + str(row['Class']) + ',' +
-                      str(row['Gene_ID']) + ',' + str(row['Distance']) + ',' +
-                      str(row['Interval']) + ',' + str(row['Signal']))
+                      str(row['Distance']) + ',' + str(row['Interval']) + ',' +
+                      str(row['Hit_Start']) + ',' + str(row['Hit_End']) + ',' +
+                      str(row['Signal']))
 
 if __name__ == '__main__':
     try:
         parser = argparse.ArgumentParser()
         parser.add_argument('-in', metavar='XML', required=True,
                             help='XML configuration file [req]')
-        parser.add_argument('-gtf', metavar='FILE', required=True,
-                            help='GTF with gene_id as first attribute [req]')
+        parser.add_argument('-bed', metavar='FILE', required=True,
+                            help='BED serving as annotations [req]')
         parser.add_argument('--signals', action='store_true', default=False,
                             help='Map BED bigwigs signals to features [false]')
         args = vars(parser.parse_args())
