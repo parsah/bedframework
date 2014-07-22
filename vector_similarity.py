152fbcef239460c714c27b9f1563d32fd20d2307
parsah <parsa.hosseini@nih.gov>
2014-07-22 12:45:15 -0400

    fixed bug that failed to correctly derive BED vector similarity scores.

--- vector_similarity.py
+++ vector_similarity.py
@@ -7,7 +7,6 @@ lengths are statistically significant.
 import argparse
 import itertools
 import numpy
-import pandas
 from src.ioutils import parse_config
 from src.model import BEDFileFactory
 from src.config import TISSUE_SPEC, UBIQUITOUS
@@ -27,7 +26,7 @@ def wilcox_test(x, y):
     if numpy.isnan(pval):  # replace NaN with a poor p-value.
         pval = 1.0
     if pval == 0.0:  # sanity checks to ensure all values are non-zero.
-        pval = numpy.finfo(numpy.float64).tiny
+        pval = numpy.finfo(numpy.float64).tiny.astype('float')
     return pval
 
 
@@ -38,30 +37,26 @@ def main(args):
     @param args: dictionary of command-line arguments.
     '''
 
-    df_out = pandas.DataFrame()  # create empty data-frame to save results into
+    print('Tissue,PValue,Length_TissSpec,Length_Ubiq')
     beds = [BEDFileFactory(elem).build() for elem in parse_config(args['in'])]
     df = concat([bed.get_data() for bed in beds])  # add to main data-frame
     ts = df[df['Class'] == TISSUE_SPEC]
     ub = df[df['Class'] == UBIQUITOUS]
     combs = itertools.product(*[df['Tissue'].unique(),
                                 df['Length'].unique(), df['Length'].unique()])
-    for tissue, len_ts, len_ub in list(combs):  # iterate over tissue, lengths
-        x = ts[(ts['Length'] == len_ts) & (ts['Tissue'] == tissue)]
+    for tiss, len_ts, len_ub in list(combs):  # iterate over tissue, lengths
+        x = ts[(ts['Length'] == len_ts) & (ts['Tissue'] == tiss)]
         x = list(itertools.chain.from_iterable(list(x['Vectors'])))  # flatten
-        y = ub[(ub['Length'] == len_ub) & (ub['Tissue'] == tissue)]
+        y = ub[(ub['Length'] == len_ub) & (ub['Tissue'] == tiss)]
         y = list(itertools.chain.from_iterable(list(y['Vectors'])))  # flatten
-        df_out = df_out.append({'Tissue': bed.get_tissue(), 'Len_Ubiq': len_ub,
-                                'PValue': wilcox_test(x, y),
-                                'Len_Tiss': len_ts}, ignore_index=True)
-    df_out.to_csv(args['out'])  # save output
+        print(tiss + ',' + str(wilcox_test(x, y)) + ',' +
+              str(len_ts) + ',' + str(len_ub))
 
 if __name__ == '__main__':
     try:
         parser = argparse.ArgumentParser()
         parser.add_argument('-in', metavar='XML', required=True,
                             help='XML configuration file [req]')
-        parser.add_argument('-out', metavar='CSV', default='./out.csv',
-                            help='Output filename [./out.csv]')
         args = vars(parser.parse_args())  # parse arguments
         main(args)
     except OSError as e:
